8add87e91a2acc4ba81b786fc633b4bb532ca8c3
Qiu <qiuosier@gmail.com>
2023-02-05 17:58:56 -0500

    Update Add-in with new template.

--- Scorpio/src/taskpane/taskpane.js
+++ Scorpio/src/taskpane/taskpane.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global document, Office */
-
 Office.onReady(info => {
   if (info.host === Office.HostType.OneNote) {
     document.getElementById("sideload-msg").style.display = "none";
